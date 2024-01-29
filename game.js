571c4b1391209d5299cb1bd78059fbf733abd8f1
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2024-01-29 15:03:36 +0200

    Files created

---
+++ game.js
@@ -0,0 +1,23 @@
+
+
+function chessBoard() {
+    let size = 8;
+    let board = "";
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if ((x + y) % 2 == 0) {
+                board += " ";
+            } else {
+                board += "#";
+            }
+        }
+        board += "\n";
+    }
+
+    return board;
+}
+
+$(document).ready(function () {
+    console.log(chessBoard());
+    $("#game").html(chessBoard());
+});
